04391fb0f1d159c727e7033f5f2a6cb63fc5bdd3
Feyribran <arnaud.vacherat@gmail.com>
2018-03-07 15:35:50 +0100

    Creation des Hands et envoie des 2 hands au dataservice

--- ng-starstone/src/app/data-service.service.ts
+++ ng-starstone/src/app/data-service.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Card} from './model/card';
+import {Zerg} from "./model/zerg";
+import {forEach} from "@angular/router/src/utils/collection";
+import {Princess} from "./model/princess";
 
 
 @Injectable()
@@ -29,4 +32,42 @@ export class DataService {
       });
 
   }
+
+
+  sendHands(zergHand: Card[], princessHand : Card[]){
+
+  }
+
+  /*sendZergHand(zerg: Zerg) {
+    let url = 'http://localhost:8080/starstone/api/general';
+
+
+
+
+    let dto = { // Data Transfer Object pour Jax-B
+      zergHand : zerg.zergHand
+    };
+
+
+    console.log('Sending topicDTO' + dto);
+
+    return this.http.post(url, dto)
+      .toPromise()
+      .then(data => console.log('Success :) ', data))
+  }*/
+
+/*  createTopic(topic: Topic) {
+    let url = 'http://localhost:8080/forum/api/topics';
+    let dto = { // Data Transfer Object pour Jax-B
+      title: topic.title,
+      admin: topic.admin
+    }
+
+    console.log('Sending topicDTO' + dto);
+
+    return this.http.post(url, dto)
+      .toPromise()
+      .then(data => console.log('Success :) ', data))
+
+  }*/
 }
--- ng-starstone/src/app/view/starstone-view/starstone-view.component.ts
+++ ng-starstone/src/app/view/starstone-view/starstone-view.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {DataService} from '../../data-service.service';
-import {Card} from '../../model/card';
+import {Card, CardOption} from '../../model/card';
 import {Princess} from '../../model/princess';
 import {Zerg} from "../../model/zerg";
 
@@ -11,10 +11,17 @@ import {Zerg} from "../../model/zerg";
 })
 export class StarstoneViewComponent implements OnInit {
 
-  zergCards: Card[];
-  princessCards: Card[];
+  count: number = 0;
+  zergCards: CardOption[];
+  zergHand: CardOption[] = [];
+  princessCards: CardOption[];
+  princessHand: CardOption[] = [];
   selectedZerg : boolean;
   selectedPrincess : boolean;
+
+  filledHand : boolean = false;
+
+  selectedCard : Card;
   princess: Princess;
   zerg: Zerg;
 
@@ -29,7 +36,7 @@ export class StarstoneViewComponent implements OnInit {
     this.selectedZerg = true;
 
     this.dataService.fetchZergDeck()
-      .then(zCards => this.zergCards = zCards);
+      .then(zCards => this.zergCards = zCards.map(c=>new CardOption(c)));
     //.then(cards => console.log('Cards : ', cards));
   }
 
@@ -37,7 +44,71 @@ export class StarstoneViewComponent implements OnInit {
     this.selectedPrincess = true;
 
     this.dataService.fetchPrincessDeck()
-      .then(pCards => this.princessCards = pCards);
-    //.then(cards => console.log('Cards : ', cards));
+      .then(pCards => this.princessCards = pCards.map(c=>new CardOption(c)))
+
   }
+
+
+  getPrincessHand(): Card[]{
+
+
+    //console.log(this.princessCards);
+    const list = this.princessCards.filter(o => o.selected).map(o=>o.card);
+
+    //console.log(list);
+
+    if (list.length == 8){
+      return list;
+    } else if (list.length < 8) {
+      console.log("You need to have 8 cards in your hand, please, keep going.")
+    } else {
+      console.log("You can't have more than 8 cards in your hand. Please drop the surplus");
+    }
+
+  }
+
+  getZergHand(): Card[]{
+
+
+    //console.log(this.zergCards);
+    const list = this.zergCards.filter(o => o.selected).map(o=>o.card);
+
+    //console.log(list);
+
+    if (list.length == 8){
+      return list;
+    } else if (list.length < 8) {
+      console.log("You need to have 8 cards in your hand, please, keep going.")
+    } else {
+      console.log("You can't have more than 8 cards in your hand. Please drop the surplus");
+    }
+
+  }
+
+  startGame(){
+    if (this.getPrincessHand() != [] && this.getZergHand() != []){
+      console.log("Start Game !!!!");
+      console.log(this.getPrincessHand());
+      console.log(this.getZergHand());
+      return this.dataService.sendHands(this.getPrincessHand(),this.getZergHand());
+    }
+  }
+
+/*  sendZergHand() {
+    if (this.princessHand.length == 8){
+      this.filledHand = true;
+
+
+    return this.dataService.sendZergHand(this.zerg)
+      .then(() => this.selectedUser.topics.push(Object.assign({}, this.createdTopic)))
+      .catch(e => alert(e.message));
+
+    }
+  }*/
 }
+/*
+createTopic() {
+  return this.dataService.createTopic(this.createdTopic)
+    .then(() => this.selectedUser.topics.push(Object.assign({}, this.createdTopic)))
+    .catch(e => alert(e.message));
+}*/
