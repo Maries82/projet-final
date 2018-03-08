de925545d71d3d1457239d44530dcd1d6409634e
Feyribran <arnaud.vacherat@gmail.com>
2018-03-08 09:10:56 +0100

    Récupère 2 hands dans le WebService et le HAndBusiness

--- projet_starstone/src/main/java/io/pax/starstone/business/HandBusiness.java
+++ projet_starstone/src/main/java/io/pax/starstone/business/HandBusiness.java
@@ -16,18 +16,45 @@ public class HandBusiness {
         return hand;
     }
 
+    public static List<Hand> getTwoHands(Hand pHand, Hand zHand){
+        List<Hand> hands = new ArrayList<>();
+
+        hands.add(pHand);
+        hands.add(zHand);
+
+        return hands;
+    }
+
 
     public static void main(String[] args) {
 
-        List<Card> cards = new ArrayList<>();
-        String color = "zerg";
-        Card card = new Card(1,1,9,9);
+        List<Card> pCards = new ArrayList<>();
+        List<Card> zCards = new ArrayList<>();
+
+        String colorZ = "zerg";
+        String colorP = "princess";
+
+        Card card = new Card(1,1,9,9, colorP);
+        Card card2 = new Card(1,2,8,9,colorP);
+
+        Card card3 = new Card(1,2,3,4,colorZ);
+        Card card4 = new Card(4,5,5,6,colorZ);
 
 
-        cards.add(card);
-        System.out.println(cards.size());
+        pCards.add(card);
+        pCards.add(card2);
+        zCards.add(card3);
+        zCards.add(card4);
 
-        System.out.println(HandBusiness.createHand(color,cards));
+        Hand pHand = new Hand(colorP, pCards);
+        Hand zHand = new Hand(colorZ, zCards);
+
+        System.out.println(pCards.size());
+        System.out.println(zCards.size());
+
+        //System.out.println(HandBusiness.createHand(color,cards));
+        System.out.println(HandBusiness.getTwoHands(pHand,zHand));
+
     }
 
 }
